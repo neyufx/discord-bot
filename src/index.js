7a1839b44f3045991175f7036c0d57a4cd1b5bd7
Djabrailov <djabra26ib@lycee-ndduroc.com>
2021-10-29 21:01:50 +0200

    v2

--- src/index.js
+++ src/index.js
@@ -122,4 +122,4 @@ bot.on('messageCreate', message => {
 bot.on('error', console.error);
 
 /* Connecte le bot avec le token fourni en paramètre */
-bot.login('token');
+bot.login(process.env.BOT_TOKEN);
