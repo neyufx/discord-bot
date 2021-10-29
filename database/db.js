7d256159f577316e27ddcad3feabb4c08316a63c
Djabrailov <djabra26ib@lycee-ndduroc.com>
2021-10-29 20:49:40 +0200

    v2

--- database/db.js
+++ database/db.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
     var pool = mysql.createPool({
         connectionLimit: 10,
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME
+        host: '153.92.220.151',
+        user: 'u463335117_brasserie',
+        password: 'Neyufx99&',
+        database: 'u463335117_brasserie',
     })
     pool.getConnection(function(err, connection) {
         if (err) throw err;
