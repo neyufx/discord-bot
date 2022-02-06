a0f2468bddde2408d9578df0807c02c2f4c24c2f
Djabrailov <djabra26ib@lycee-ndduroc.com>
2022-02-06 01:55:57 +0100

    v10

--- src/index.js
+++ src/index.js
@@ -33,6 +33,8 @@ bot.on('messageCreate', message => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
     const gerantRole = message.member.roles.cache.some(role => role.name === 'Gérants');// rôle
+    const patronRole = message.member.roles.cache.some(role => role.name === 'Patron');// rôle
+    const coPatronRole = message.member.roles.cache.some(role => role.name === 'Co-Patron');// rôle
     /* Si la commande user */
     if(gerantRole){
     if(command === 'user'){ // Commande !user <nomrp> <nomsteam> @taguser
@@ -163,7 +165,9 @@ bot.on('messageCreate', message => {
     }
     else if (command === 'classement')
     {
+        if(patronRole || coPatronRole){
         bot.commands.get('classement').execute(message,args);
+        }
     }
     else if (command === 'salon')
     {
@@ -176,7 +180,9 @@ bot.on('messageCreate', message => {
     }
     else if (command === 'classement10')
     {
+        if(patronRole || coPatronRole){
         bot.commands.get('classement10').execute(message,args);
+        }
     }
 }
 
