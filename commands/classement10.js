06b467ef5f7b91066ee387ac6db49f417e5f5ce0
Djabrailov <djabra26ib@lycee-ndduroc.com>
2022-03-27 13:00:00 +0200

    v10.1

--- commands/classement10.js
+++ commands/classement10.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, MessageEmbed, Intents } = require('discord.js');
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
 const { Guild } = require('discord.js');
 const db = require('../database/db.js');
